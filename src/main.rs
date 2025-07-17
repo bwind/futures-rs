3e22c245ac31c0b7a33ca06fff38c58cc4b1ec3d
Bas Wind <bas@baswind.nl>
2025-07-17 15:19:54 +0200

    chore: move closure to fn + clippy pedantic fixes

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,33 @@ struct CounterFuture {
 
 const ITERATIONS: u32 = 10;
 
+fn do_work(
+    thread_waker: &Arc<Mutex<Option<Waker>>>,
+    waker_ref: &Arc<AtomicU32>,
+    poll_count: &Arc<AtomicU32>,
+) {
+    // Each thread sleeps for the same duration, so that we intentionally introduce a race condition
+    // and ensure that multiple threads try to wake up the task
+    thread::sleep(Duration::from_millis(100));
+
+    // Atomically increase the counter without using a Mutex<T>
+    poll_count.fetch_add(1, Ordering::SeqCst);
+
+    // Not each thread will get Some because they are competing for the waker
+    if let Some(waker) = thread_waker.lock().unwrap().take() {
+        waker_ref.fetch_add(1, Ordering::SeqCst);
+        println!(
+            "Waker: {waker:?}, ref: {}, poll_count: {}",
+            waker_ref.load(Ordering::SeqCst),
+            poll_count.load(Ordering::SeqCst),
+        );
+        waker.wake();
+    }
+}
+
 impl CounterFuture {
     fn new() -> Self {
-        let waker: Arc<Mutex<Option<Waker>>> = Default::default();
+        let waker: Arc<Mutex<Option<Waker>>> = Arc::default();
         let waker_ref = Arc::new(AtomicU32::new(0));
         let poll_count = Arc::new(AtomicU32::new(0));
 
@@ -29,25 +53,7 @@ impl CounterFuture {
             let thread_waker_ref = waker_ref.clone();
             let poll_count = poll_count.clone();
 
-            thread::spawn(move || {
-                // Each thread sleeps for the same duration, so that we intentionally introduce a race condition
-                // and ensure that multiple threads try to wake up the task
-                thread::sleep(Duration::from_millis(100));
-
-                // Atomically increase the counter without using a Mutex<T>
-                poll_count.fetch_add(1, Ordering::SeqCst);
-
-                // Not each thread will get Some because they are competing for the waker
-                if let Some(waker) = thread_waker.lock().unwrap().take() {
-                    thread_waker_ref.fetch_add(1, Ordering::SeqCst);
-                    println!(
-                        "Waker: {waker:?}, ref: {}, poll_count: {}",
-                        thread_waker_ref.load(Ordering::SeqCst),
-                        poll_count.load(Ordering::SeqCst),
-                    );
-                    waker.wake();
-                }
-            });
+            thread::spawn(move || do_work(&thread_waker, &thread_waker_ref, &poll_count));
         }
         Self {
             waker,
@@ -64,10 +70,7 @@ impl Future for CounterFuture {
         let mut waker = self.waker.lock().unwrap();
         let count = self.poll_count.load(Ordering::Relaxed);
         let waker_ref = self.waker_ref.load(Ordering::SeqCst);
-        println!(
-            "Poll count from poll(): {}, waker_ref: {}",
-            count, waker_ref
-        );
+        println!("Poll count from poll(): {count}, waker_ref: {waker_ref}");
         if count == ITERATIONS {
             Poll::Ready(())
         } else {
